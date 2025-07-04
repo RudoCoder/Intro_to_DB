c2d122c8bd2621b592d5adaf94e8312b4f78eeed
RudoCoder <rudomakuwe@gmail.com>
2025-07-04 10:59:50 +0200

    Adding Items to table

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSERT INTO customers (id, first_name, last_name, email)
-VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com');
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Cole', 'Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
